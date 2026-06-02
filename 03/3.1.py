4f2d086cca6f6b5679fda31476f3f126c799e1e6
IvanMk9 <iveselchak9@gmail.com>
2026-06-02 16:07:53 +0300

    homework for lesson 03

---
+++ 03/3.1.py
@@ -0,0 +1,22 @@
+user_number_1 = input("Enter first number: ")
+if not user_number_1.isdigit():
+   print("Invalid operation, use only numbers for math operations")
+   exit()
+user_number_2 = input("Enter second number: ")
+if not user_number_2.isdigit():
+   print("Invalid operation, use only numbers for math operations")
+   exit()
+user_operation_type = input("Enter operation type (+, -, *, /): ")
+
+if user_operation_type == "/" and int(user_number_2) == 0:
+    print("You can't divide by 0,  enter another number")
+elif user_operation_type == "+":
+    print("Result:", int(user_number_1) + int(user_number_2))
+elif user_operation_type == "-":
+    print("Result:", int(user_number_1) - int(user_number_2))
+elif user_operation_type == "*":
+    print("Result:", int(user_number_1) * int(user_number_2))
+elif user_operation_type == "/":
+    print("Result:", int(user_number_1) / int(user_number_2))
+else:
+    print("Invalid operation, try again")
